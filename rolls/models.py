7e1c5d94d873e058bc74ef8ffeb66e136ff8dff6
mswinton <mswinton@gmail.com>
2025-12-15 00:44:56 -0500

    page 2

--- rolls/models.py
+++ rolls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=30)
+
+class CharStat(models.Model):
+    character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
+    name = models.CharField(max_length=10)
+    value = models.PositiveSmallIntegerField(default=0)
