91806c916bd82790b9299c3210642d129e0ec006
mswinton <mswinton@gmail.com>
2025-12-21 17:49:33 -0500

    part 2 and some cleanup

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'rolls.apps.RollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- rolls/models.py
+++ rolls/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
 class Character(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+    attr_dex = models.PositiveSmallIntegerField(default=1)
+    attr_stam = models.PositiveSmallIntegerField(default=1)
+    attr_str = models.PositiveSmallIntegerField(default=1)
+    attr_app = models.PositiveSmallIntegerField(default=1)
+    attr_cha = models.PositiveSmallIntegerField(default=1)
+    attr_manip = models.PositiveSmallIntegerField(default=1)
+    attr_per = models.PositiveSmallIntegerField(default=1)
+    attr_int = models.PositiveSmallIntegerField(default=1)
+    attr_wits = models.PositiveSmallIntegerField(default=1)
 
-class CharStat(models.Model):
+class Ability(models.Model):
     character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
-    name = models.CharField(max_length=10)
+    ability = models.CharField(max_length=20)
     value = models.PositiveSmallIntegerField(default=0)
+    caste_or_favored = models.BooleanField(default=0)
+
+class Intimacy(models.Model):
+    character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
+    intimacy = models.TextField(max_length=200)
+
+class Specialty(models.Model):
+    ability_id = models.ForeignKey(Ability, on_delete=models.CASCADE)
+    specialty = models.TextField(max_length=20)
